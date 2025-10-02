e49f9fd4563eaffb5b7d770e2c5c33fbc55b87be
Sahareior Sijan <sahareiors@gmail.com>
2025-10-02 13:29:57 +0600

    fixed color issue

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -17,7 +17,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-green-400 transition-all"
+      className="h-full w-full flex-1 bg-[#0CBF03] transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
